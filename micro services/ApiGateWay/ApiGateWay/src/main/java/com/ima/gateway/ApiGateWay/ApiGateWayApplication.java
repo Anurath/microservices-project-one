e0454e87bce0fa733736b837b791865a77407c3e
Anurath <anurathwaghmode2018@gmail.com>
2025-12-17 11:08:20 +0530

    Api Gateway Created
    Api Gateway Created

---
+++ micro services/ApiGateWay/ApiGateWay/src/main/java/com/ima/gateway/ApiGateWay/ApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.ima.gateway.ApiGateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+
+}
